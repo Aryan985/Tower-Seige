22264902b1ab14f286a204cdc82f33e626391fe4
Aryan985 <71887957+Aryan985@users.noreply.github.com>
2020-10-28 12:31:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,116 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+
+
+
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  
+
+ground = new Ground(600,600,900,20);
+stand1 = new Ground(390,300,250,10);
+stand2 = new Ground(700,200,200,10);
+
+//level 1
+
+block1 = new Block(300,275,30,40);
+block2 = new Block(330,275,30,40);
+block3 = new Block(360,275,30,40);
+block4 = new Block(390,275,30,40);
+block5 = new Block(420,275,30,40);
+block6 = new Block(450,275,30,40);
+block7 = new Block(480,275,30,40);
+//level 2
+block8 = new Block(330,235,30,40);
+block9 = new Block(360,235,30,40);
+block10 = new Block(390,235,30,40);
+block11= new Block(420,235,30,40);
+block12 = new Block(450,235,30,40);
+//level 3
+block13 = new Block(360,195,30,40);
+block14 = new Block(390,195,30,40);
+block15 = new Block(420,195,30,40);
+block16 = new Block(390,155,30,40);
+
+//level 1
+//for second stand
+
+block1 = new Block(640,175,30,40);
+block2 = new Block(670,175,30,40);
+block3 = new Block(700,175,30,40);
+block4 = new Block(730,175,30,40);
+block5 = new Block(760,175,30,40);
+//level 2
+block5 = new Block(670,135,30,40);
+block6 = new Block(700,135,30,40);
+block7 = new Block(730,135,30,40);
+block8 = new Block(700,95,30,40);
+
+polygon = Bodies.circle(50,200,20);
+World.add(world,polygon);
+
+
+slingshot = new Slingshot(this.polygon,{x:100,y:200});
+
+
+
+
+  
+}
+
+function draw() {
+  background(56,44,44);  
+  Engine.update(engine);
+
+ground.display();
+stand1.disply();
+stand2.display();
+fill("skyblue");
+block1.display();
+block2.display();
+block3.display();
+block4.display();
+block5.display();
+block6.display();
+block7.display();
+fill("pink");
+block8.display();
+block9.display();
+block10.display();
+block11.display();
+block12.display();
+fill("yellow");
+block13.display();
+block14.display();
+block15.display();
+fill("grey");
+block16.display();
+fill("yellow")
+block1.display();
+block2.display();
+block3.display();
+block4.display();
+block5.display();
+fill("skyblue");
+block6.display();
+block7.display();
+block8.display();
+fill("pink");
+block9.display();
+slingshot.display();
+}
+
+function mouseDragged(){
+Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY})
+
+}
+
+function mouseReleased(){
+  slingshot.fly();
+}
